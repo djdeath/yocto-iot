cc15b2f196d2c24ddd1e092123321adc9961eee1
Paul Eggleton <paul.eggleton@linux.intel.com>
2014-02-07 17:00:29 +0000

    devkit-release.sh: run do_build instead of do_rootfs for images
    This ensures we actually get a kernel for example.
    
    Signed-off-by: Paul Eggleton <paul.eggleton@linux.intel.com>

--- scripts/devkit-release.sh
+++ scripts/devkit-release.sh
@@ -45,7 +45,7 @@ generate_cache_data() {
     rm -rf $TMPDIR
     bitbake $TARGETS
     bitbake -S $TARGETS
-    cat locked-sigs.inc | sed -e '/do_rootfs/d' -e '/do_bootimg/d' -e '/do_populate_sdk/d' > $LOCKED_SIGS_FILE
+    cat locked-sigs.inc | sed -e '/do_rootfs/d' -e '/do_bootimg/d' -e '/do_populate_sdk/d' -e '/do_build/d' > $LOCKED_SIGS_FILE
     rm locked-sigs.inc
     echo "Launching Hob to populate caches, will close automatically"
     HOB_POPULATE_CACHES_ONLY=1 hob-iot
@@ -54,13 +54,13 @@ generate_cache_data() {
 # Full distro
 LOCKED_SIGS_FILE="../meta-iot-devkit/conf/distro/include/iot-devkit-locked-sigs.inc"
 export DISTRO="iot-devkit-locked"
-TARGETS="iot-devkit-image:do_rootfs iot-devkit-image:do_populate_sdk iot-devkit-prof-dev-image:do_rootfs iot-devkit-prof-dev-image:do_populate_sdk iot-devkit-prof-dev-image:do_rootfs iot-devkit-prof-dev-image:do_populate_sdk"
+TARGETS="iot-devkit-image:do_build iot-devkit-image:do_populate_sdk iot-devkit-prof-dev-image:do_build iot-devkit-prof-dev-image:do_populate_sdk iot-devkit-prof-dev-image:do_build iot-devkit-prof-dev-image:do_populate_sdk"
 generate_cache_data
 
 # SPI distro
 LOCKED_SIGS_FILE="../meta-iot-devkit/conf/distro/include/iot-devkit-spi-locked-sigs.inc"
 export DISTRO="iot-devkit-spi-locked"
-TARGETS="iot-devkit-spi-image:do_rootfs iot-devkit-spi-image:do_populate_sdk"
+TARGETS="iot-devkit-spi-image:do_build iot-devkit-spi-image:do_populate_sdk"
 generate_cache_data
 
 # Symlink all other distros host sstate subdirs to the current distro
